3626a9345b18161a4651a25a69c8378190423976
Kai-46 <kz298@cornell.edu>
2022-02-04 15:02:27 -0500

    rename conda env

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@
 
 bash ./preprocess_sfm/install_colmapforvissat.sh
 
-conda create -y -n satellite_sfm python=3.8 && conda activate satellite_sfm
+conda create -y -n SatelliteSfM python=3.8 && conda activate SatelliteSfM
 pip install numpy matplotlib opencv-python pyexr open3d tqdm icecream imageio-ffmpeg
 pip install utm pyproj pymap3d
 conda install -y -c conda-forge gdal
